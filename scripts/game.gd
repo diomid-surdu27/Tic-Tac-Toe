6c5c9d4752295bb509ccf2d82f93c7bf31212218
diomid-surdu27 <diomid.su@gmail.com>
2026-01-29 21:02:34 +0200

    Turn based sys with generated cells (better)

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+var cell_scene = preload("res://scenes/cell.tscn")
+var cells = []
+var board_size = Vector2i(3,3)
+
 var turn = Globals.TurnState.TURN_X
 
 func _process(_delta):
@@ -9,16 +13,17 @@ func _process(_delta):
 		get_tree().reload_current_scene()
 
 func _ready():
-	$Cell.clicked.connect(_on_cell_clicked)
-	$Cell2.clicked.connect(_on_cell_clicked)
-	$Cell3.clicked.connect(_on_cell_clicked)
-	$Cell4.clicked.connect(_on_cell_clicked)
-	$Cell5.clicked.connect(_on_cell_clicked)
-	$Cell6.clicked.connect(_on_cell_clicked)
-	$Cell7.clicked.connect(_on_cell_clicked)
-	$Cell8.clicked.connect(_on_cell_clicked)
-	$Cell9.clicked.connect(_on_cell_clicked)
-#Logday 39
+	for x in range(board_size.x):
+		cells.append([])
+		for y in range(board_size.y):
+			var cell = cell_scene.instantiate()
+			add_child(cell)
+			cells[x].append(cell)
+			cell.global_position = Vector2(75 + x*100, 250 + y*100)
+			cell.clicked.connect(_on_cell_clicked)
+			
+	%Board.global_position = Vector2(75 + 150, 250 + 150)
+	
 func _on_cell_clicked(cell):
 	if cell.fill_state != Globals.FillState.FILL_NONE:
 		return
